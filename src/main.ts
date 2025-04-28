85419d721110e9bdb3bd9f40fe30ba3093a2f9f7
Dedwison <dedwison@gmail.com>
2025-04-28 16:24:44 -0400

    validar tipado de variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { envs } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,9 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  // console.log({port: envs.port})
+
+  await app.listen( envs.port );
+  console.log(`Server running on port: ${ envs.port }`)
 }
 bootstrap();
